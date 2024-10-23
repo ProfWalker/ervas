e79ddf390e8394d84cdb9832449d7ba7a8dee6dd
ProfWalker <walkertadeu@prof.educacao.sp.gov.br>
2024-10-23 13:33:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'ERVAS',
+    'Gengibre: Zingiber officinale',
+    'Conhecido por suas propriedades medicinais, o gengibre é anti-inflamatório, antioxidante e digestivo. Comumente usado para aliviar náuseas, dores musculares e problemas estomacais, também é um ingrediente versátil na culinária, presente em pratos salgados, doces e bebidas. Além disso, suas propriedades antioxidantes o tornam um componente popular em cosméticos.'
+)
+
+criaCartao(
+    'ERVAS',
+    'Boldo: Peumus boldus',
+    'O boldo é amplamente utilizado para tratar problemas digestivos e do fígado. Suas propriedades ajudam na digestão, especialmente em casos de má digestão ou dores no estômago, e promovem o bom funcionamento hepático. O chá de boldo é um dos remédios caseiros mais comuns para tratar distúrbios gástricos.'
+)
+
+criaCartao(
+    'ERVAS',
+    'Erva-doce: Foeniculum vulgare',
+    'Famosa por suas propriedades calmantes e digestivas, a erva-doce é comumente usada em chás para aliviar problemas como cólicas, gases e má digestão. Também é conhecida por sua ação anti-inflamatória e por ser suave, sendo indicada inclusive para bebês em casos de desconforto digestivo.'
+)
+
+criaCartao(
+    'ERVAS',
+    'Camomila: Matricaria chamomilla',
+    'A camomila é uma planta tradicionalmente usada para acalmar, aliviar ansiedade e melhorar o sono. Além de suas propriedades relaxantes, ela tem ação anti-inflamatória e auxilia no tratamento de problemas gastrointestinais leves, como gastrite. O chá de camomila é popular para tratar insônia e reduzir o estresse.'
+)
+
+criaCartao(
+    'ERVAS',
+    'Babosa: Aloe vera',
+    ' A babosa é amplamente utilizada em tratamentos estéticos e medicinais devido às suas propriedades cicatrizantes, anti-inflamatórias e hidratantes. Aplicada em queimaduras, feridas e na hidratação da pele e dos cabelos, a babosa também é usada para fortalecer o sistema imunológico e auxiliar na saúde digestiva quando ingerida.'
+)
+
+criaCartao(
+    'ERVAS',
+    'Alcachofra: Cynara scolymus',
+    'A alcachofra é uma planta conhecida por suas propriedades que auxiliam na saúde do fígado, promovem a digestão e reduzem o colesterol. Rica em antioxidantes, é usada para ajudar na digestão de gorduras e no controle do peso. Seu extrato é comum em tratamentos para desintoxicação e controle de triglicerídeos.'
+)
